18bc074cd3f8167231fddc05ed047a6b3a2d8047
chenxy <xiaoyang-chen@xxx.com>
2022-10-09 23:01:42 +0800

    feat: add EncryptByPubKeyWithPrefixBeforeBase64 and DecryptByPrivateKeyWithPrefixAfterBase64

--- age-wrap.go
+++ age-wrap.go
@@ -1,6 +1,8 @@
 package x25519
 
 import (
+	"encoding/base64"
+
 	"github.com/xiaoyang-chen/x25519/internal/age"
 
 	"github.com/pkg/errors"
@@ -11,6 +13,10 @@ type AgeX25519 interface {
 	GeneratePubKeyAndPrivateKey(pubPrefix, privatePrefix string) (pub, private string, err error)
 	EncryptByPubKeyWithPrefix(in []byte, publicKey, prefix string) (out []byte, err error)
 	DecryptByPrivateKeyWithPrefix(in []byte, privateKey, prefix string) (out []byte, err error)
+	// EncryptByPubKeyWithPrefixBeforeBase64 encrypt and then base64 encode, if encoding is nil, it will use base64.RawStdEncoding, this method is usually used to handle messy code
+	EncryptByPubKeyWithPrefixBeforeBase64(in []byte, publicKey, prefix string, encoding *base64.Encoding) (out []byte, err error)
+	// DecryptByPrivateKeyWithPrefixAfterBase64 base64 decode and then decrypt, if encoding is nil, it will use base64.RawStdEncoding, this method is usually used to handle messy code
+	DecryptByPrivateKeyWithPrefixAfterBase64(in []byte, privateKey, prefix string, encoding *base64.Encoding) (out []byte, err error)
 }
 
 func NewAgeX25519() AgeX25519 { return new(ageX25519Wrap) }
@@ -41,5 +47,33 @@ func (*ageX25519Wrap) EncryptByPubKeyWithPrefix(in []byte, publicKey, prefix str
 func (*ageX25519Wrap) DecryptByPrivateKeyWithPrefix(in []byte, privateKey, prefix string) (out []byte, err error) {
 
 	out, err = age.DecryptByX25519PrivateKey(in, privateKey, prefix)
+	return
+}
+
+func (*ageX25519Wrap) EncryptByPubKeyWithPrefixBeforeBase64(in []byte, publicKey, prefix string, encoding *base64.Encoding) (out []byte, err error) {
+
+	if out, err = age.EncryptByX25519PublicKey(in, publicKey, prefix); err == nil {
+		if encoding == nil {
+			encoding = base64.RawStdEncoding
+		}
+		var base64Out = make([]byte, encoding.EncodedLen(len(out)))
+		encoding.Encode(base64Out, out)
+		out = base64Out
+	}
+	return
+}
+
+func (*ageX25519Wrap) DecryptByPrivateKeyWithPrefixAfterBase64(in []byte, privateKey, prefix string, encoding *base64.Encoding) (out []byte, err error) {
+
+	if encoding == nil {
+		encoding = base64.RawStdEncoding
+	}
+	var base64In = make([]byte, encoding.DecodedLen(len(in)))
+	var n int
+	if n, err = encoding.Decode(base64In, in); err != nil {
+		err = errors.Wrap(err, "base64 decode fail")
+		return
+	}
+	out, err = age.DecryptByX25519PrivateKey(base64In[:n], privateKey, prefix)
 	return
 }
