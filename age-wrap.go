b6ce7a220057c02ce55d710bfa4e106381c0262b
chenxy <xiaoyang-chen@xxx.com>
2022-11-03 16:28:33 +0800

    feat: add encrypt and decrypt all file in dir

--- age-wrap.go
+++ age-wrap.go
@@ -2,10 +2,14 @@ package x25519
 
 import (
 	"encoding/base64"
+	"io/fs"
+	"os"
+	"path/filepath"
 
 	"github.com/xiaoyang-chen/x25519/internal/age"
 
 	"github.com/pkg/errors"
+	"github.com/xiaoyang-chen/rotate"
 )
 
 type ageX25519Wrap struct{}
@@ -17,6 +21,10 @@ type AgeX25519 interface {
 	EncryptByPubKeyWithPrefixBeforeBase64(in []byte, publicKey, prefix string, encoding *base64.Encoding) (out []byte, err error)
 	// DecryptByPrivateKeyWithPrefixAfterBase64 base64 decode and then decrypt, if encoding is nil, it will use base64.RawStdEncoding, this method is usually used to handle messy code
 	DecryptByPrivateKeyWithPrefixAfterBase64(in []byte, privateKey, prefix string, encoding *base64.Encoding) (out []byte, err error)
+	// EncryptAllFileInDirByPubKeyWithPrefixBeforeBase64ThenRotateWrite encrypt file and base64 encode and rotate write in dir one by one, if encoding is nil, it will use base64.RawStdEncoding
+	EncryptAllFileInDirByPubKeyWithPrefixBeforeBase64ThenRotateWrite(dir, backupDir, publicKey, prefix string, encoding *base64.Encoding) (err error)
+	// DecryptAllFileInDirByPrivateKeyWithPrefixAfterBase64ThenRotateWrite base64 decode and decrypt file and rotate write in dir one by one, if encoding is nil, it will use base64.RawStdEncoding
+	DecryptAllFileInDirByPrivateKeyWithPrefixAfterBase64ThenRotateWrite(dir, backupDir, privateKey, prefix string, encoding *base64.Encoding) (err error)
 }
 
 func NewAgeX25519() AgeX25519 { return new(ageX25519Wrap) }
@@ -75,5 +83,85 @@ func (*ageX25519Wrap) DecryptByPrivateKeyWithPrefixAfterBase64(in []byte, privat
 		return
 	}
 	out, err = age.DecryptByX25519PrivateKey(base64In[:n], privateKey, prefix)
+	return
+}
+
+func (wrap *ageX25519Wrap) EncryptAllFileInDirByPubKeyWithPrefixBeforeBase64ThenRotateWrite(dir, backupDir, publicKey, prefix string, encoding *base64.Encoding) (err error) {
+
+	if encoding == nil {
+		encoding = base64.RawStdEncoding
+	}
+	// get all file in dir
+	var files []fs.DirEntry
+	if files, err = os.ReadDir(dir); err != nil {
+		err = errors.Wrap(err, "read dir fail")
+		return
+	}
+	var fileName string
+	var fileBody []byte
+	var filePath string
+	var encryptData []byte
+	var writer = rotate.RotateOnWrite{BackupDir: backupDir}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		fileName = f.Name()
+		filePath = filepath.Join(dir, fileName)
+		if fileBody, err = os.ReadFile(filePath); err != nil {
+			err = errors.Wrapf(err, "read file fail, filePath: %s", filePath)
+			return
+		}
+		if encryptData, err = wrap.EncryptByPubKeyWithPrefixBeforeBase64(fileBody, publicKey, prefix, encoding); err != nil {
+			err = errors.WithMessagef(err, "encrypt file fail, filePath: %s", filePath)
+			return
+		}
+		// rotate write
+		writer.Filename = filePath
+		if _, err = writer.Write(encryptData); err != nil {
+			err = errors.WithMessagef(err, "write encryptData to file fail, filePath: %s", filePath)
+			return
+		}
+	}
+	return
+}
+
+func (wrap *ageX25519Wrap) DecryptAllFileInDirByPrivateKeyWithPrefixAfterBase64ThenRotateWrite(dir, backupDir, privateKey, prefix string, encoding *base64.Encoding) (err error) {
+
+	if encoding == nil {
+		encoding = base64.RawStdEncoding
+	}
+	// get all file in dir
+	var files []fs.DirEntry
+	if files, err = os.ReadDir(dir); err != nil {
+		err = errors.Wrap(err, "read dir fail")
+		return
+	}
+	var fileName string
+	var fileBody []byte
+	var filePath string
+	var decryptData []byte
+	var writer = rotate.RotateOnWrite{BackupDir: backupDir}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		fileName = f.Name()
+		filePath = filepath.Join(dir, fileName)
+		if fileBody, err = os.ReadFile(filePath); err != nil {
+			err = errors.Wrapf(err, "read file fail, filePath: %s", filePath)
+			return
+		}
+		if decryptData, err = wrap.DecryptByPrivateKeyWithPrefixAfterBase64(fileBody, privateKey, prefix, encoding); err != nil {
+			err = errors.WithMessagef(err, "decrypt file fail, filePath: %s", filePath)
+			return
+		}
+		// rotate write
+		writer.Filename = filePath
+		if _, err = writer.Write(decryptData); err != nil {
+			err = errors.WithMessagef(err, "write decryptData to file fail, filePath: %s", filePath)
+			return
+		}
+	}
 	return
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/xiaoyang-chen/rotate v0.0.6
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 )
 
